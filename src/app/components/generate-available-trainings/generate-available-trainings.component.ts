880bd0937b1b2b462bc5e8ab023be5e39dd8264d
avigailvkn <144704507+avigailvkn@users.noreply.github.com>
2024-09-12 02:20:24 +0300

    Update the visual code in component generate-available-trainings

--- src/app/components/generate-available-trainings/generate-available-trainings.component.ts
+++ src/app/components/generate-available-trainings/generate-available-trainings.component.ts
@@ -1,24 +1,33 @@
-import { NONE_TYPE } from '@angular/compiler';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-generate-available-trainings',
   templateUrl: './generate-available-trainings.component.html',
-  styleUrls: ['./generate-available-trainings.component.scss']
+  styleUrls: ['./generate-available-trainings.component.scss'],
+  providers: [MessageService]
 })
 export class GenerateAvailableTrainingsComponent {
   rangeDates?: Date[];
-  date1?:Date;
-  date2?:Date;
+  numberOfMonths: number = 2; // ברירת מחדל - שני חודשים
 
+  today: Date = new Date(); // תאריך נוכחי
+  isWeekend:boolean=false;
+  stateOptions: any[] = [
+    { label: 'כן', value: true },
+    { label: 'לא', value: false }
+  ];
   
+  value: string = ''; // Or set default value as per your requirement
+
+
   closeCalendarOnBlur(event: Event) {
     const calendarElement = document.querySelector('.p-calendar');
 
     if (event instanceof FocusEvent) {
         if (calendarElement && event.relatedTarget && !calendarElement.contains(event.relatedTarget as Node)) {
             (calendarElement as HTMLElement).classList.remove('p-calendar-visible');
+           
         }
     } else {
         console.warn('Event is not of type FocusEvent');
@@ -35,7 +44,7 @@ onDateSelect(event: any) {
 
     if (diffInDays > 30) {
       this.messageService.add({
-        severity: 'warn',
+        severity: 'error',
         summary: 'בחירת תאריכים לא תקינה',
         detail: 'ניתן לבחור טווח של עד 30 ימים בלבד'
       });
@@ -44,7 +53,120 @@ onDateSelect(event: any) {
     }
   }
 }
+isCalendarInline: boolean = false;
 
+// פונקציה לפתיחה/סגירה של לוח השנה בלחיצות חוזרות
+toggleCalendar() {
+  this.isCalendarInline = !this.isCalendarInline;
+}
+
+// // ==============================================
+isCalendarDisabled:boolean=false;
+// // פונקציה לבדיקת מצב ה-rangeDates וכיבוי או הפעלת p-selectButton
+checkSelectButtonState() {
+//   if (this.rangeDates && this.rangeDates.length === 2) {
+//     this.isSelectButtonDisabled = false;
+//   } else if (this.rangeDates && this.rangeDates.length === 1 && this.isWeekend) {
+//     this.isCalendarDisabled = true;
+//     this.isSelectButtonDisabled = true;
+//   } else {
+//     this.isSelectButtonDisabled = true;
+//     this.isCalendarDisabled = false;
+//   }
+}
+
+
+tmp?:boolean;
+onWeekendToggle() {
+  if(this.isWeekend==null){
+    this.isWeekend=false;
+  }
+  // אם יש תאריך אחד ועד סופ"ש
+  if (this.isWeekend && this.rangeDates && 
+    this.rangeDates.length === 2&&this.rangeDates[1]===null) {
+    this.isCalendarDisabled = true;
+  }
+    //כל שאר המקרים
+   else {
+    this.isCalendarDisabled = false;
+  }
+  this.tmp= this.isWeekend && this.rangeDates && 
+  this.rangeDates.length === 2&&this.rangeDates[1]===null;
+}
+
+
+// פונקציה שמפעילה לוגיקה כלשהי בעת לחיצה על "ביצוע"
+submitForGenerateAvailableTraining() {
+  if(!this.rangeDates)
+  this.messageService.add({
+    severity: 'error',
+    summary: 'שגיאה',
+    detail: 'יש לבחור לפחות תאריך אחד לפני ביצוע הפעולה'
+  });
+   else {
+    if(this.rangeDates && this.rangeDates.length === 2){
+      if(this.rangeDates[0]!=null){
+        if(this.rangeDates[1]){
+          //יש לך 2 תאריכים תקינים
+          if(this.isWeekend){
+            this.messageService.add({
+              severity: 'error',
+              summary: 'שגיאה',
+              detail: 'טווח תאריכים נבחר,אנא בטל את ההגדרה לסופ"ש' 
+            });
+          }
+          else{
+            this.messageService.add({
+              severity: 'success',
+              summary: 'ביצוע הצליח',
+              detail: 'הפעולה הושלמה בהצלחה!'
+            });
+          }
+
+        }
+        else{
+          //הוגדר רק תאריך 1
+          if(this.isWeekend){
+            this.messageService.add({
+              severity: 'success',
+              summary: 'ביצוע הצליח',
+              detail: 'הפעולה הושלמה בהצלחה!'
+            });
+          }
+          //תאריך 1 בלי סופ"ש
+          else{
+            this.messageService.add({
+              severity: 'error',
+              summary: 'שגיאה',
+              detail: 'אנא בחר תאריך נוסף או סמן את תיבת הסופ"ש' 
+            });
+          }
+        }
+        
+      //פה צריך לסגור את החלון צף
+      //שליחה לשרת
+    }
+      
+      
+    }
+    
+  }
+}
+ngOnInit() {
+  this.updateCalendarMonths(window.innerWidth);
+}
+@HostListener('window:resize', ['$event'])
+onResize(event: Event) {
+  this.updateCalendarMonths(window.innerWidth);
+}
+
+updateCalendarMonths(width: number) {
+  if (width <= 768) {
+    this.numberOfMonths = 1; // הצג חודש אחד בלבד במסכים קטנים
+  } else {
+    this.numberOfMonths = 2; // הצג שני חודשים במסכים גדולים
+  }
+}
 }
 
 
